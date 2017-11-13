4a3cda388a5443a04dd84f9141092f53a8facd49
Sergienko Anton <sergienkoanton@mail.ru>
2017-11-14 00:34:09 +0700

    Add styles for content.

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-    $(window).scroll(function(){
+    $(window).scroll(function() {
         var bo = $(this).scrollTop();
-        if ( bo >= 50) {
-          $("#logo").addClass("shrink");
+        if (bo >= 50) {
+            $("#logo").addClass("shrink");
+        } else {
+            $("#logo").removeClass("shrink");
         }
-        else {
-          $("#logo").removeClass("shrink");
-        }
-      });
+    });
+
+    $('.spoiler-text').hide();
+    $('.spoiler').click(function() {
+        $(this).toggleClass("folded").toggleClass("unfolded").next().slideToggle();
+    });
 });
