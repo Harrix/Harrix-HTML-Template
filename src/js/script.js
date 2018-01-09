dc0cac1f567f5fd23fb5d512eaa55800c7c27e84
Sergienko Anton <sergienkoanton@mail.ru>
2018-01-09 20:58:37 +0700

    Replace $ to jQuery. To ensure compatibility.

--- src/js/script.js
+++ src/js/script.js
@@ -4,34 +4,34 @@
  * Licensed under MIT (https://github.com/Harrix/Harrix-HTML-Template/blob/master/LICENSE)
  */
 
-$(function() {
+jQuery(function() {
     //Run syntax highlighting
-    $('pre code').each(function(i, block) {
+    jQuery('pre code').each(function(i, block) {
         hljs.highlightBlock(block);
     });
 
     //Change the size of the logo when scroll
-    $(window).scroll(function() {
-        var bo = $(this).scrollTop();
+    jQuery(window).scroll(function() {
+        var bo = jQuery(this).scrollTop();
         if (bo >= 50) {
-            $("#logo").addClass("shrink");
+            jQuery("#logo").addClass("shrink");
         } else {
-            $("#logo").removeClass("shrink");
+            jQuery("#logo").removeClass("shrink");
         }
     });
 
     //Hide or show the button "Back to up"
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-            $('#back-to-top').fadeIn();
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > 200) {
+            jQuery('#back-to-top').fadeIn();
         } else {
-            $('#back-to-top').fadeOut();
+            jQuery('#back-to-top').fadeOut();
         }
     });
 
     //When you press the "Back to up" animated transition
-    $('#back-to-top').click(function() {
-        $('html, body').animate({
+    jQuery('#back-to-top').click(function() {
+        jQuery('html, body').animate({
             scrollTop: 0
         }, 800);
         return false;
@@ -41,11 +41,11 @@ $(function() {
     iFrameResize({});
 
     //TODO: Working with the left pane
-    //if ($('*').is('#sidebar')) forNavigationDrawer();//
+    //if (jQuery('*').is('#sidebar')) forNavigationDrawer();//
 
     //Gallery
     changeSizeFiguraInPhotoswipeGallery();
-    $(window).resize(function() {
+    jQuery(window).resize(function() {
         //При изменении размеров окна тоже нужно поменять размеры изображений в галереях 
         changeSizeFiguraInPhotoswipeGallery();
 
@@ -53,19 +53,19 @@ $(function() {
         //Принудительно показываем боковую панель при увеличении размера окна
         //forcedDisplaySidebar();
     });
-    $('.photoswipe_gallery').masonry({
+    jQuery('.photoswipe_gallery').masonry({
         // options
         itemSelector: '.msnry_item',
         fitWidth: true,
     });
 
     //Smooth scrolling to anchors
-    $('a[href*="#"]:not([href="#"])').click(function() {
+    jQuery('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            var target = jQuery(this.hash);
+            target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
-                $('html, body').animate({
+                jQuery('html, body').animate({
                     scrollTop: target.offset().top
                 }, 800);
                 return false;
@@ -77,69 +77,69 @@ $(function() {
 //TODO:
 /*function forNavigationDrawer() {
   //Клик на гамбургер
-  $('.hamburger').click(function() {
-    $('#sidebar').toggleClass("sidebar-open");
-    $('#dark').toggleClass("dark-open");
-    $('#for-swipe').toggleClass("for-swipe-open");
-    $('#for-swipe-in').toggleClass("for-swipe-in-open");
+  jQuery('.hamburger').click(function() {
+    jQuery('#sidebar').toggleClass("sidebar-open");
+    jQuery('#dark').toggleClass("dark-open");
+    jQuery('#for-swipe').toggleClass("for-swipe-open");
+    jQuery('#for-swipe-in').toggleClass("for-swipe-in-open");
   });
   //При нажатии на затемнение убираем левую панель
-  $('#dark').click(function(){
-     $('.hamburger').click();
+  jQuery('#dark').click(function(){
+     jQuery('.hamburger').click();
   });
-  $('#for-swipe').click(function(){
-     $('.hamburger').click();
+  jQuery('#for-swipe').click(function(){
+     jQuery('.hamburger').click();
   });
-  $('#for-swipe-in').click(function(){
-     $('.hamburger').click();
+  jQuery('#for-swipe-in').click(function(){
+     jQuery('.hamburger').click();
   });
   
   //Swipes
   var forSwipe = document.getElementById('for-swipe');
   var hammertimeForSwipe = new Hammer(forSwipe);
   hammertimeForSwipe.on('swipeleft', function(ev) {
-      if ( $('#for-swipe').hasClass('for-swipe-open') == true )
-      $('.hamburger').click();
+      if ( jQuery('#for-swipe').hasClass('for-swipe-open') == true )
+      jQuery('.hamburger').click();
   });
   hammertimeForSwipe.on('swiperight', function(ev) {
-      if ( $('#for-swipe').hasClass('for-swipe-open') == false )
-      $('.hamburger').click();
+      if ( jQuery('#for-swipe').hasClass('for-swipe-open') == false )
+      jQuery('.hamburger').click();
   });
   
   var forSwipeIn = document.getElementById('for-swipe-in');
   var hammertimeForSwipeIn = new Hammer(forSwipeIn);
   hammertimeForSwipeIn.on('swipeleft', function(ev) {
-      if ( $('#for-swipe-in').hasClass('for-swipe-in-open') == true )
-      $('.hamburger').click();
+      if ( jQuery('#for-swipe-in').hasClass('for-swipe-in-open') == true )
+      jQuery('.hamburger').click();
   });
   hammertimeForSwipeIn.on('swiperight', function(ev) {
-      if ( $('#for-swipe-in').hasClass('for-swipe-in-open') == false )
-      $('.hamburger').click();
+      if ( jQuery('#for-swipe-in').hasClass('for-swipe-in-open') == false )
+      jQuery('.hamburger').click();
   });
   
   var dark = document.getElementById('dark');
   var hammertimeDark = new Hammer(dark);
   hammertimeDark.on('swipe', function(ev) {
-      $('.hamburger').click();
+      jQuery('.hamburger').click();
   });  
 }*/
 
 //TODO:
 /*function forcedDisplaySidebar() {
   //Function forced showing in the sidebar when you increase the window size
-  var width_content = $("body").width();
+  var width_content = jQuery("body").width();
   
   if (width_content > 992) {
-    $('#sidebar').removeClass("sidebar-open").show();
-    $('#dark').removeClass("dark-open").hide();
-    $('#for-swipe').removeClass("for-swipe-open").hide();
-    $('#for-swipe-in').removeClass("for-swipe-in-open").hide();
+    jQuery('#sidebar').removeClass("sidebar-open").show();
+    jQuery('#dark').removeClass("dark-open").hide();
+    jQuery('#for-swipe').removeClass("for-swipe-open").hide();
+    jQuery('#for-swipe-in').removeClass("for-swipe-in-open").hide();
   }
 }*/
 
 function changeSizeFiguraInPhotoswipeGallery() {
     //The counting function of the width of the pictures in the galleries
-    var width_content = $(".content-with-gallery").width();
+    var width_content = jQuery(".content-with-gallery").width();
     var w_figura = (width_content - 40) / 3;
-    $(".msnry_item").width(w_figura);
+    jQuery(".msnry_item").width(w_figura);
 }
