9b34ec49c9d563eaee5ed8bb795f7be9eded753a
Sergienko Anton <sergienkoanton@mail.ru>
2018-01-29 03:50:51 +0700

    Add the contents of the generated javascript.

--- src/js/script.js
+++ src/js/script.js
@@ -44,6 +44,36 @@ jQuery(function() {
         }, 800);
     });
 
+    var ToC =
+  "<nav role='navigation' class='table-of-contents'>" +
+    "<h2>On this page:</h2>" +
+    "<ul>";
+
+var newLine, el, title, link;
+
+$("article h3").each(function() {
+
+  el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+
+  newLine =
+    "<li>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</li>";
+
+  ToC += newLine;
+
+});
+
+ToC +=
+   "</ul>" +
+  "</nav>";
+
+$(".all-questions").prepend(ToC);
+
     //Turn on the auto-size iframe which is on the page
     iFrameResize({});
 
