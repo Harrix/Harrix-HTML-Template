68ab6c8a99518898a576b1d89e812e55199af017
Sergienko Anton <sergienkoanton@mail.ru>
2018-02-17 00:58:01 +0700

    Add the npm packages that were used in the previous version.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import moment from 'moment';
+//import moment from 'moment';
 console.log("HelloTot! from JavaScript!");
 console.log(moment().startOf('day').fromNow());
 var name = "Bob", time = "today";
-console.log(`Hello ${name}, how ar465464646e you ${time}?`);
+console.log(`Hello ${name}, how ar465466e you ${time}?`);
