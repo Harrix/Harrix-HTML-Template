ae345641fbabd3159f928189599776603e5880a7
Sergienko Anton <sergienkoanton@mail.ru>
2018-02-17 16:44:54 +0700

    Delete unnecessary lines in the code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 //import moment from 'moment';
 console.log("HelloTot! from JavaScript!");
-console.log(moment().startOf('day').fromNow());
 var name = "Bob", time = "today";
 console.log(`Hello ${name}, how ar465466e you ${time}?`);
