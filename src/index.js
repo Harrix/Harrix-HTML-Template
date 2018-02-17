44f3bd18d47214d0894326aa4d6ee711d0ea3fc2
Sergienko Anton <sergienkoanton@mail.ru>
2018-02-17 22:31:16 +0700

    Add .scss files.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 //import moment from 'moment';
+//import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
 console.log("HelloTot! from JavaScript!");
 var name = "Bob", time = "today";
 console.log(`Hello ${name}, how ar465466e you ${time}?`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,54 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      filename: './src/bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['env']
-            }
+  entry: [
+    './src/index.js',
+    './src/scss/style.scss'
+  ],
+  output: {
+    filename: './dist/bundle.js'
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
           }
         }
-      ]
-    }
-  };
+      },
+      {
+        test: /\.(sass|scss)$/,
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+        use: ExtractTextPlugin.extract({
+          //fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(woff2?|ttf|otf|eot|svg)$/,
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+          outputPath: '.dist/font'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'dist/style.bundle.css',
+      allChunks: true,
+    }),
+  ],
+};
