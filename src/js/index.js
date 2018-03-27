3c1b5a3ef2aba426cab6c1bf03fd43311e162d9c
Sergienko Anton <sergienkoanton@mail.ru>
2018-03-28 02:36:40 +0700

    Delete unnecessary lines in the code

--- src/js/index.js
+++ src/js/index.js
@@ -11,46 +11,6 @@ import fontawesomeFreeBrands from '@fortawesome/fontawesome-free-brands'
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Dropdowns
-  
-    const $metalinks = getAll('#meta a');
-  
-    if ($metalinks.length > 0) {
-      $metalinks.forEach($el => {
-        $el.addEventListener('click', event => {
-          event.preventDefault();
-          const target = $el.getAttribute('href');
-          const $target = document.getElementById(target.substring(1));
-          $target.scrollIntoView(true);
-          // window.history.replaceState(null, document.title, `${window.location.origin}${window.location.pathname}${target}`);
-          return false;
-        });
-      });
-    }
-  
-    // Dropdowns
-  
-    const $dropdowns = getAll('.dropdown:not(.is-hoverable)');
-  
-    if ($dropdowns.length > 0) {
-      $dropdowns.forEach($el => {
-        $el.addEventListener('click', event => {
-          event.stopPropagation();
-          $el.classList.toggle('is-active');
-        });
-      });
-  
-      document.addEventListener('click', event => {
-        closeDropdowns();
-      });
-    }
-  
-    function closeDropdowns() {
-      $dropdowns.forEach($el => {
-        $el.classList.remove('is-active');
-      });
-    }
-  
     // Toggles
   
     const $burgers = getAll('.burger');
@@ -73,104 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const $modalButtons = getAll('.modal-button');
     const $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
   
-    if ($modalButtons.length > 0) {
-      $modalButtons.forEach($el => {
-        $el.addEventListener('click', () => {
-          const target = $el.dataset.target;
-          const $target = document.getElementById(target);
-          rootEl.classList.add('is-clipped');
-          $target.classList.add('is-active');
-        });
-      });
-    }
-  
-    if ($modalCloses.length > 0) {
-      $modalCloses.forEach($el => {
-        $el.addEventListener('click', () => {
-          closeModals();
-        });
-      });
-    }
-  
-    document.addEventListener('keydown', event => {
-      const e = event || window.event;
-      if (e.keyCode === 27) {
-        closeModals();
-        closeDropdowns();
-      }
-    });
-  
-    function closeModals() {
-      rootEl.classList.remove('is-clipped');
-      $modals.forEach($el => {
-        $el.classList.remove('is-active');
-      });
-    }
-  
-    // Clipboard
-  
-    const $highlights = getAll('.highlight');
-    let itemsProcessed = 0;
-  
-    if ($highlights.length > 0) {
-      $highlights.forEach($el => {
-        const copyEl = '<button class="button is-small bd-copy">Copy</button>';
-        const expandEl = '<button class="button is-small bd-expand">Expand</button>';
-        $el.insertAdjacentHTML('beforeend', copyEl);
-  
-        const $parent = $el.parentNode;
-        if ($parent && $parent.classList.contains('bd-is-more')) {
-          const showEl = '<button class="bd-show"><div><span class="icon"><i class="fas fa-code"></i></span> <strong>Show code</strong></div></button>';
-          $el.insertAdjacentHTML('beforeend', showEl);
-        } else if ($el.firstElementChild.scrollHeight > 480 && $el.firstElementChild.clientHeight <= 480) {
-          $el.insertAdjacentHTML('beforeend', expandEl);
-        }
-  
-        itemsProcessed++;
-        if (itemsProcessed === $highlights.length) {
-          addHighlightControls();
-        }
-      });
-    }
-  
-    function addHighlightControls() {
-      const $highlightButtons = getAll('.highlight .bd-copy, .highlight .bd-expand');
-  
-      $highlightButtons.forEach($el => {
-        $el.addEventListener('mouseenter', () => {
-          $el.parentNode.classList.add('bd-is-hovering');
-        });
-  
-        $el.addEventListener('mouseleave', () => {
-          $el.parentNode.classList.remove('bd-is-hovering');
-        });
-      });
-  
-      const $highlightExpands = getAll('.highlight .bd-expand');
-  
-      $highlightExpands.forEach($el => {
-        $el.addEventListener('click', () => {
-          $el.parentNode.firstElementChild.style.maxHeight = 'none';
-        });
-      });
-  
-      const $highlightShows = getAll('.highlight .bd-show');
-  
-      $highlightShows.forEach($el => {
-        $el.addEventListener('click', () => {
-          $el.parentNode.parentNode.classList.remove('bd-is-more-clipped');
-        });
-      });
-    }
-  
-    setTimeout(() => {
-      new Clipboard('.bd-copy', {
-        target: trigger => {
-          return trigger.previousElementSibling.firstElementChild;
-        }
-      });
-    }, 100);
-  
+    
     // Functions
   
     function getAll(selector) {
@@ -284,12 +147,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
       ticking = true;
     });
-  
-  });  
 
-  document.addEventListener('DOMContentLoaded', () => {
-
-    const rootEl = document.documentElement;
     const navbarTopEl = document.getElementById('navbar');
     const navbarBottomEl = document.getElementById('navbarBottom');
     const fixBottomEl = document.getElementById('navbarFixBottom');
