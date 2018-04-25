8bdd13b62a4f87777f15a061fc926823cbfe47b3
Sergienko Anton <sergienkoanton@mail.ru>
2018-04-25 12:47:28 +0700

    Add custom icons for fontawesome

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,8 @@ import fontawesomeFreeSolid from '@fortawesome/fontawesome-free-solid'
 import fontawesomeFreeRegular from '@fortawesome/fontawesome-free-regular'
 import fontawesomeFreeBrands from '@fortawesome/fontawesome-free-brands'
 
+import faCustomIcons from './_fah.js'
+
 document.addEventListener('DOMContentLoaded', () => {
     initNavbar(50, 300);
     initSearchPanel();
