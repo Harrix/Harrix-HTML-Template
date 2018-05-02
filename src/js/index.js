28ad53256e03634ef881f93290015a66e07e2d2b
Sergienko Anton <sergienkoanton@mail.ru>
2018-05-03 04:26:23 +0700

    Experiment with search panel

--- src/js/index.js
+++ src/js/index.js
@@ -57,6 +57,7 @@ function initNavbar(scrollTopLogoShrink, scrollTopNavbarHide) {
 
 function initSearchPanel() {
     const navbarMenu = document.getElementById('h-navbar-menu');
+    const navbarMenuEnd = document.getElementById('h-navbar-menu-end');
     const buttonSearchOpen = document.getElementById('h-button-search-panel-open');
     const buttonSearchClose = document.getElementById('h-button-search-panel-close');
     const hdivsearch = document.getElementById('h-div-search');
@@ -64,7 +65,12 @@ function initSearchPanel() {
     buttonSearchOpen.addEventListener('click', () => {
         //navbarMenu.classList.add('h-has-visible-search-panel');
         //navbarMenu.classList.add('h-has-visible-search-panel');
-        hinputsearch.classList.toggle('h-www');
+        //hinputsearch.classList.toggle('h-www');
+        var ww = navbarMenuEnd.clientWidth;
+        //hinputsearch.style.width = 500;
+        console.log(" w = " + navbarMenuEnd.clientWidth);
+        hinputsearch.style.width = navbarMenuEnd.clientWidth + "px";
+        navbarMenuEnd.classList.toggle('hideItems');
     });
     buttonSearchClose.addEventListener('click', () => {
         navbarMenu.classList.remove('h-has-visible-search-panel');
