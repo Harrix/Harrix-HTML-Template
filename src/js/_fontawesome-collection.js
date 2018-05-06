2a65fe94c3b55a5ba5be091ff20c0e8f2cbb05c8
Sergienko Anton <sergienkoanton@mail.ru>
2018-05-07 01:11:16 +0700

    Style. Correction code

--- src/js/_fontawesome-collection.js
+++ src/js/_fontawesome-collection.js
@@ -1,11 +1,10 @@
 import fontawesome from '@fortawesome/fontawesome'
 
-//import fontawesomeFreeRegular from '@fortawesome/fontawesome-free-regular'
+import faCustomIcons from './_fontawesome-harrix-icons.js'
 
+//import fontawesomeFreeRegular from '@fortawesome/fontawesome-free-regular'
 import faSearch from '@fortawesome/fontawesome-free-solid/faSearch'
 import faGithub from '@fortawesome/fontawesome-free-brands/faGithub'
-
-import faCustomIcons from './_fontawesome-harrix-icons.js'
 
 fontawesome.library.add(faSearch)
 fontawesome.library.add(faGithub)
