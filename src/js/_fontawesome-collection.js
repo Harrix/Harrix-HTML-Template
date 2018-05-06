90cf07aaa684a74cd0c8dc1fbe7cfcb82fcd67ee
Sergienko Anton <sergienkoanton@mail.ru>
2018-05-07 00:31:18 +0700

    Rename _fah.js to _fontawesome-harrix-icons.js

--- src/js/_fontawesome-collection.js
+++ src/js/_fontawesome-collection.js
@@ -4,4 +4,4 @@ import fontawesomeFreeSolid from '@fortawesome/fontawesome-free-solid'
 import fontawesomeFreeRegular from '@fortawesome/fontawesome-free-regular'
 import fontawesomeFreeBrands from '@fortawesome/fontawesome-free-brands'
 
-import faCustomIcons from './_fah.js'
+import faCustomIcons from './_fontawesome-harrix-icons.js'
