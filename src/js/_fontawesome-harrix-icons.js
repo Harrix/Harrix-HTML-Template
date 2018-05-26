5f8146726f03374a5ea94d44a1f2553b9e5c56e9
Sergienko Anton <sergienkoanton@mail.ru>
2018-05-26 22:25:05 +0700

    Style. Beautify code

--- src/js/_fontawesome-harrix-icons.js
+++ src/js/_fontawesome-harrix-icons.js
@@ -1,117 +1,128 @@
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global['fontawesome-free-brands'] = {})));
-}(this, (function (exports) { 'use strict';
+(function(global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+        typeof define === 'function' && define.amd ? define(['exports'], factory) :
+        (factory((global['fontawesome-free-brands'] = {})));
+}(this, (function(exports) {
+    'use strict';
 
-var _WINDOW = {};
-try {
-  if (typeof window !== 'undefined') _WINDOW = window;
-  
-} catch (e) {}
+    var _WINDOW = {};
+    try {
+        if (typeof window !== 'undefined') _WINDOW = window;
 
-var _ref = _WINDOW.navigator || {};
-var _ref$userAgent = _ref.userAgent;
-var userAgent = _ref$userAgent === undefined ? '' : _ref$userAgent;
+    } catch (e) {}
 
-var WINDOW = _WINDOW;
+    var _ref = _WINDOW.navigator || {};
+    var _ref$userAgent = _ref.userAgent;
+    var userAgent = _ref$userAgent === undefined ? '' : _ref$userAgent;
 
-var IS_IE = ~userAgent.indexOf('MSIE') || ~userAgent.indexOf('Trident/');
+    var WINDOW = _WINDOW;
 
-var NAMESPACE_IDENTIFIER = '___FONT_AWESOME___';
+    var IS_IE = ~userAgent.indexOf('MSIE') || ~userAgent.indexOf('Trident/');
 
-var PRODUCTION = function () {
-  try {
-    return process.env.NODE_ENV === 'production';
-  } catch (e) {
-    return false;
-  }
-}();
+    var NAMESPACE_IDENTIFIER = '___FONT_AWESOME___';
 
-var oneToTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var oneToTwenty = oneToTen.concat([11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+    var PRODUCTION = function() {
+        try {
+            return process.env.NODE_ENV === 'production';
+        } catch (e) {
+            return false;
+        }
+    }();
 
-var RESERVED_CLASSES = ['xs', 'sm', 'lg', 'fw', 'ul', 'li', 'border', 'pull-left', 'pull-right', 'spin', 'pulse', 'rotate-90', 'rotate-180', 'rotate-270', 'flip-horizontal', 'flip-vertical', 'stack', 'stack-1x', 'stack-2x', 'inverse', 'layers', 'layers-text', 'layers-counter'].concat(oneToTen.map(function (n) {
-  return n + 'x';
-})).concat(oneToTwenty.map(function (n) {
-  return 'w-' + n;
-}));
+    var oneToTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var oneToTwenty = oneToTen.concat([11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
 
-function bunker(fn) {
-  try {
-    fn();
-  } catch (e) {
-    if (!PRODUCTION) {
-      throw e;
+    var RESERVED_CLASSES = ['xs', 'sm', 'lg', 'fw', 'ul', 'li', 'border', 'pull-left', 'pull-right', 'spin', 'pulse', 'rotate-90', 'rotate-180', 'rotate-270', 'flip-horizontal', 'flip-vertical', 'stack', 'stack-1x', 'stack-2x', 'inverse', 'layers', 'layers-text', 'layers-counter'].concat(oneToTen.map(function(n) {
+        return n + 'x';
+    })).concat(oneToTwenty.map(function(n) {
+        return 'w-' + n;
+    }));
+
+    function bunker(fn) {
+        try {
+            fn();
+        } catch (e) {
+            if (!PRODUCTION) {
+                throw e;
+            }
+        }
     }
-  }
-}
 
-var w = WINDOW || {};
+    var w = WINDOW || {};
 
-if (!w[NAMESPACE_IDENTIFIER]) w[NAMESPACE_IDENTIFIER] = {};
-if (!w[NAMESPACE_IDENTIFIER].styles) w[NAMESPACE_IDENTIFIER].styles = {};
-if (!w[NAMESPACE_IDENTIFIER].hooks) w[NAMESPACE_IDENTIFIER].hooks = {};
-if (!w[NAMESPACE_IDENTIFIER].shims) w[NAMESPACE_IDENTIFIER].shims = [];
+    if (!w[NAMESPACE_IDENTIFIER]) w[NAMESPACE_IDENTIFIER] = {};
+    if (!w[NAMESPACE_IDENTIFIER].styles) w[NAMESPACE_IDENTIFIER].styles = {};
+    if (!w[NAMESPACE_IDENTIFIER].hooks) w[NAMESPACE_IDENTIFIER].hooks = {};
+    if (!w[NAMESPACE_IDENTIFIER].shims) w[NAMESPACE_IDENTIFIER].shims = [];
 
-var namespace = w[NAMESPACE_IDENTIFIER];
+    var namespace = w[NAMESPACE_IDENTIFIER];
 
-var _extends = Object.assign || function (target) {
-  for (var i = 1; i < arguments.length; i++) {
-    var source = arguments[i];
+    var _extends = Object.assign || function(target) {
+        for (var i = 1; i < arguments.length; i++) {
+            var source = arguments[i];
 
-    for (var key in source) {
-      if (Object.prototype.hasOwnProperty.call(source, key)) {
-        target[key] = source[key];
-      }
+            for (var key in source) {
+                if (Object.prototype.hasOwnProperty.call(source, key)) {
+                    target[key] = source[key];
+                }
+            }
+        }
+        return target;
+    };
+
+    function define(prefix, icons) {
+        var normalized = Object.keys(icons).reduce(function(acc, iconName) {
+            var icon = icons[iconName];
+            var expanded = !!icon.icon;
+
+            if (expanded) {
+                acc[icon.iconName] = icon.icon;
+            } else {
+                acc[iconName] = icon;
+            }
+            return acc;
+        }, {});
+
+        if (typeof namespace.hooks.addPack === 'function') {
+            namespace.hooks.addPack(prefix, normalized);
+        } else {
+            namespace.styles[prefix] = _extends({}, namespace.styles[prefix] || {}, normalized);
+        }
+
+        if (prefix === 'fas') {
+            define('fa', icons);
+        }
     }
-  }
-  return target;
-};
 
-function define(prefix, icons) {
-  var normalized = Object.keys(icons).reduce(function (acc, iconName) {
-    var icon = icons[iconName];
-    var expanded = !!icon.icon;
-
-    if (expanded) {
-      acc[icon.iconName] = icon.icon;
-    } else {
-      acc[iconName] = icon;
-    }
-    return acc;
-  }, {});
-
-  if (typeof namespace.hooks.addPack === 'function') {
-    namespace.hooks.addPack(prefix, normalized);
-  } else {
-    namespace.styles[prefix] = _extends({}, namespace.styles[prefix] || {}, normalized);
-  }
-
-  if (prefix === 'fas') {
-    define('fa', icons);
-  }
-}
-
-var prefix = "fah";
-var faHarrix = { prefix: 'fah', iconName: 'harrix', icon: [512, 512, [], "e000", "M441.3 210.8C460.6 289 428 371.2 361 415.2l-62.2-142.6-59.6 25.9L301 441.1c-78.1 19.3-160.3-13.3-204.4-80.4L36.2 387C101 496.3 238.4 542.6 358 490.8s179.2-184 143.7-305.9l-60.4 25.9zM154 21.2C34.7 73.1-25.2 205.3 10.3 327.1l60.4-26.3C51.8 223 84 140.5 151.4 96.4L213.2 239l59.6-25.9L211 70.5c78.1-19.3 160.3 13.3 204.4 80.4l60.4-26.3C410.9 15.7 273.6-30.6 154 21.2z"] };
-var faClose = { prefix: 'fah', iconName: 'close', icon: [512, 512, [], "e001", "M295.6 256l130.1-130.1c4.7-4.7 4.7-12.3 0-17l-22.6-22.6c-4.7-4.7-12.3-4.7-17 0L256 216.4 125.9 86.3c-4.7-4.7-12.3-4.7-17 0l-22.6 22.6c-4.7 4.7-4.7 12.3 0 17L216.4 256 86.3 386.1c-4.7 4.7-4.7 12.3 0 17l22.6 22.6c4.7 4.7 12.3 4.7 17 0L256 295.6l130.1 130.1c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17L295.6 256z"] };
+    var prefix = "fah";
+    var faHarrix = {
+        prefix: 'fah',
+        iconName: 'harrix',
+        icon: [512, 512, [], "e000", "M441.3 210.8C460.6 289 428 371.2 361 415.2l-62.2-142.6-59.6 25.9L301 441.1c-78.1 19.3-160.3-13.3-204.4-80.4L36.2 387C101 496.3 238.4 542.6 358 490.8s179.2-184 143.7-305.9l-60.4 25.9zM154 21.2C34.7 73.1-25.2 205.3 10.3 327.1l60.4-26.3C51.8 223 84 140.5 151.4 96.4L213.2 239l59.6-25.9L211 70.5c78.1-19.3 160.3 13.3 204.4 80.4l60.4-26.3C410.9 15.7 273.6-30.6 154 21.2z"]
+    };
+    var faClose = {
+        prefix: 'fah',
+        iconName: 'close',
+        icon: [512, 512, [], "e001", "M295.6 256l130.1-130.1c4.7-4.7 4.7-12.3 0-17l-22.6-22.6c-4.7-4.7-12.3-4.7-17 0L256 216.4 125.9 86.3c-4.7-4.7-12.3-4.7-17 0l-22.6 22.6c-4.7 4.7-4.7 12.3 0 17L216.4 256 86.3 386.1c-4.7 4.7-4.7 12.3 0 17l22.6 22.6c4.7 4.7 12.3 4.7 17 0L256 295.6l130.1 130.1c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17L295.6 256z"]
+    };
 
 
-var icons$1 = {
-  faHarrix: faHarrix,
-  faClose: faClose,
-};
+    var icons$1 = {
+        faHarrix: faHarrix,
+        faClose: faClose,
+    };
 
-bunker(function () {
-  define('fah', icons$1);
-});
+    bunker(function() {
+        define('fah', icons$1);
+    });
 
-exports['default'] = icons$1;
-exports.prefix = prefix;
-exports.faHarrix = faHarrix;
-exports.faClose = faClose;
+    exports['default'] = icons$1;
+    exports.prefix = prefix;
+    exports.faHarrix = faHarrix;
+    exports.faClose = faClose;
 
-Object.defineProperty(exports, '__esModule', { value: true });
+    Object.defineProperty(exports, '__esModule', {
+        value: true
+    });
 
 })));
