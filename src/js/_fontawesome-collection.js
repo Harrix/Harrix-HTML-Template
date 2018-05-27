d758496aaba73bb8694f638d82d901aeb2a38e4b
Sergienko Anton <sergienkoanton@mail.ru>
2018-05-27 23:44:20 +0700

    Update fortawesome

--- src/js/_fontawesome-collection.js
+++ src/js/_fontawesome-collection.js
@@ -1,9 +1,17 @@
-import fontawesome from '@fortawesome/fontawesome'
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
 
-import faCustomIcons from './_fontawesome-harrix-icons.js'
+//import faCustomIcons from './_fontawesome-harrix-icons.js'
 
-import faSearch from '@fortawesome/fontawesome-free-solid/faSearch'
-import faGithub from '@fortawesome/fontawesome-free-brands/faGithub'
+import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch';
+import { faFileArchive } from '@fortawesome/free-solid-svg-icons/faFileArchive';
+import { faDownload } from '@fortawesome/free-solid-svg-icons/faDownload';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 
-fontawesome.library.add(faSearch);
-fontawesome.library.add(faGithub);
+library.add(
+	faSearch,
+    faFileArchive,
+    faDownload,
+    faGithub
+);
+
+dom.watch()
