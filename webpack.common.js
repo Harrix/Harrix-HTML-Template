6aa89da404a19f42aca469943840c19dc66394f4
Sergienko Anton <sergienkoanton@mail.ru>
2018-06-01 04:18:35 +0700

    Style. Correction code

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const fs = require('fs')
+const fs = require('fs');
 var webpack = require('webpack');
 
 function generateHtmlPlugins(templateDir) {
