107075862d309e951ac72af134ce6370528333cd
Sergienko Anton <sergienkoanton@mail.ru>
2018-06-03 20:09:30 +0700

    Delete comments from bundle of js

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,23 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: "source-map",
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        uglifyOptions: {
+          output: {
+            comments: false,
+          }
+        },
+      })
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin('dist')
   ]
