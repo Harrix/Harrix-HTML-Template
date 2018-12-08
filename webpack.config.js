0d207e40abcd62ff231a12d1aebb0179dc669bf9
Sergienko Anton <sergienkoanton@mail.ru>
2018-12-08 18:44:55 +0700

    Style. Beautify code

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const fs = require("fs");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 function generateHtmlPlugins(templateDir) {
